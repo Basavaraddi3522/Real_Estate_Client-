60f8bd90c0c90d20cafcb7d647e31b2856ca6583
Basavaraddi-Mulimani <basavaraddi3522@gmail.com>
2022-10-20 16:14:13 +0530

    client side imp

---
+++ src/components/headerpage/Header.js
@@ -0,0 +1,27 @@
+import "./Header.css";
+import React from 'react';
+import { CgChevronDown } from "react-icons/cg";
+import { BiUser } from "react-icons/bi";
+
+
+const Header = () => {
+  const userId = "PPID011";
+  const userName = "Manuuu";
+  return (
+    <>
+      <div className="headercontainer">
+        <div className="userid">USER ID:{window.localStorage.getItem('email')} </div>
+
+        <div className="dropdown">
+          <BiUser />
+          {window.localStorage.getItem('email')}
+          <CgChevronDown />
+          
+        </div>
+      </div>
+
+      <div className="headerbtmline"></div>
+    </>
+  );
+};
+export default Header;
